a83ff96afef01c75556874b9a92aaa15015969fc
Theo Roque <theoroque95@gmail.com>
2022-07-13 14:45:23 +0800

    initial commit

---
+++ src/store/slices/userSlice.ts
@@ -0,0 +1,47 @@
+import {createSlice, Draft, PayloadAction} from "@reduxjs/toolkit"
+
+export interface UserState {
+    email: string;
+    name: string;
+}
+
+/* 
+    Default state object with initial values
+*/
+const initialState: UserState = {
+    email: 'theoroque95@gmail.com',
+    name: 'Theo Roque'
+} as const;
+
+/* 
+    Create a sliec as a reducer containing actions
+
+    In this example, actions are included in the slice. It is fine and can be
+    changed based on needs. 
+*/
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setName: (
+            state: Draft<typeof initialState>,
+            action: PayloadAction<typeof initialState.name>
+        ) => {
+            state.name = action.payload;
+        },
+        setEmail: (
+            state: Draft<typeof initialState>,
+            action: PayloadAction<typeof initialState.email>
+        ) => {
+            state.email = action.payload;
+        }
+    }
+});
+
+// A small helper of user state for `useSelector` function
+export const getUserState = (state: { user: UserState }) => state.user;
+
+// Exports all actions
+export const { setName, setEmail } = userSlice.actions;
+
+export default userSlice.reducer;
